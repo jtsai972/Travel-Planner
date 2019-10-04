f0a422f5524436a97c49afa9087905c2c9f20e31
J Tsai <jtsai972@gmail.com>
2019-10-04 11:53:29 -0500

    finished settng up the base ajax calls

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,6 +7,11 @@
  *  flight API search: 
  *  https://developers.amadeus.com/self-service/category/air/api-doc/flight-low-fare-search/api-reference
  * 
+ * hotel API search:
+ * https://developers.amadeus.com/self-service/category/hotel/api-doc/hotel-search/api-reference
+ * 
+ * resource for flight and hotel search:
+ * https://documenter.getpostman.com/view/2672636/RWEcPfuJ?version=latest#8196c48f-30f9-4e3b-8590-e22f96da8326
  * */
 
 /* ====================================
@@ -29,10 +34,8 @@ const dbAuth = database.ref('/authentication');
  * Non-database Global Variables
  * ==================================== */
 
-var key = { googleHotelKey: "AIzaSyBP4mjguEehRKYpkuPYgvEn0mLeNnpJZxw" };
-
-dbAuth.push(key);
-
+// var key = {  };
+// dbAuth.push(key);
 
 /* ====================================
  * Initialization of document
@@ -157,3 +160,53 @@ function hotelAPI(queryValues) {
     });
 }
 
+function restaurantAPI(queryValues) {
+    //base url for the API
+    var queryBaseURL = "https://api.yelp.com/v3/"; 
+
+    var queryURL = 
+        queryBaseURL + "businesses/search?" + queryValues;
+
+    //So I'm using an API that requires an authentication token and it really doesn't want you to commit your key and stuff anywhere
+    
+    //referencing firebase in order to get keys from there (jtsai)
+    //This is crap security, but still better than nothing. (jtsai)
+    dbAuth.once("value", function(snapshot) {
+        var cid; //creating variables for keys
+        cid = snapshot.child('yelpKey').val();
+        //console.log(`cId: ${cid} cSec: ${csec}`);
+
+        //using the keys from firebase to get a token (this token expires so I figured it'd be safer to make sure it's called whenever you need this flightAPI query) (jtsai)
+        var auth = {
+            "async": true,
+            "crossDomain": true,
+            "url": queryURL,
+            "method": "GET",
+            "headers": {
+                "Authorization": "Bearer" + cid,
+                "cache-control": "no-cache"
+            }
+        }
+
+        $.ajax(auth).done(function (response) {
+            token = response;
+            console.log(token);
+
+            //once authentication is done:
+            // Finally starting the actual ajax query for flight data(jtsai)
+            $.ajax({
+                url : queryURL,
+                method : "GET"
+            }).then( function(response) {
+                var queryResult = response;
+                //variables you want will go here
+                //Example for printing out multiple 
+                // for(let i=0; i < queryResult.data[i]) {
+                //     imgSrc = queryResult.data[i].<more path here>
+                //     <more data and variables here>
+                //     print();
+                // }
+            });
+        });
+    })
+}
